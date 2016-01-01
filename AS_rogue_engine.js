111ff5e5343aa614f377f7296f921f4be56e7a5f
Andy Wong <awong@cogolabs.com>
2016-01-01 00:41:06 -0500

    added notes

--- AS_rogue_engine.js
+++ AS_rogue_engine.js
@@ -25,6 +25,12 @@ NOTES:
 Tick 0 - Everyone can move as long as there is no combatants
 Tick 1 - The player can move, combatants cannot
 Tick 2 - Combatants move, player cannot
+
+TODO:
+Refine Event Queue
+On Tick 2, all events are fired off in the event queue, one at a time,
+until there are none left. When there are no more combatant events,
+the Tick is set to 1.
 */
 
 (function() {
